95f9c429b8791aed3e52d350805d51fa5a2f3c46
mbiagichan <biagi.chan@gmail.com>
2026-02-15 22:22:10 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import SeenApp from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <SeenApp />
   </React.StrictMode>,
 )
