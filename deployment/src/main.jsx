22d28cb8b0b2d0e2d7578fe0433b7eceaceb0b28
mbiagichan <biagi.chan@gmail.com>
2026-02-16 17:29:48 -0700

    Delete deployment/src/main.jsx

--- deployment/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import SeenApp from './App'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <SeenApp />
-  </React.StrictMode>,
-)
